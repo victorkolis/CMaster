c85c1ea33d64f97b584df94b84d834ddfe74a3f4
Victor Kolis <>
2020-11-09 18:46:29 -0300

    20201109

---
+++ JasonFedin/04 - Basic Concepts/_02_Variables.c
@@ -0,0 +1,14 @@
+/*
+ * _02_Variables.c
+ *
+ *  Created on: Nov 9, 2020
+ *      Author: victordematos
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	int age = 26;
+	age = 27;
+}
---
+++ JasonFedin/04 - Basic Concepts/_03_BasicDataTypes.c
@@ -0,0 +1,19 @@
+/*
+ * _03_BasicDataTypes.c
+ *
+ *  Created on: Nov 9, 2020
+ *      Author: victordematos
+ *
+ */
+
+#include <stdio.h>
+#include <stdbool.h> /* Because of this header */
+
+int main()
+{
+	float heightInMeters = 17.5;
+	double salary = 2300.7;
+	_Bool isMarried = 1; /* This is the standard way of creating a boolean value in C*/
+	bool isMale = true; /* I can use this */
+
+}
