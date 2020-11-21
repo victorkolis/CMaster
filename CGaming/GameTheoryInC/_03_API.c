c386dfdcf18d0438e211cd537c3adeb76cc28cf5
Victor Kolis <>
2020-11-20 22:04:28 -0300

    20201120

---
+++ CGaming/GameTheoryInC/_03_API.c
@@ -0,0 +1,15 @@
+//
+//  _03_API.c
+//  GameTheoryInC
+//
+//  Created by VictorKolis on 20/11/20.
+//
+
+#include <stdio.h>
+#include "SDL/SDL.h"
+
+int main(int argc, char *argv[]){
+    
+    
+    return 0;
+}
