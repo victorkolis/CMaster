3969b194c880d32b6211093488f64f53387be17f
Victor Kolis <>
2020-11-20 23:31:52 -0300

    20201120

---
+++ Vlad/02 - Conditions/_17_odd_challange.c
@@ -0,0 +1,27 @@
+//
+//  _17_odd_challange.c
+//  Vlad
+//
+//  Created by VictorKolis on 20/11/20.
+//
+
+#include <stdio.h>
+
+int main(){
+    
+    int userInput;
+    
+    
+    printf("Please enter a number >>> ");
+    scanf("%d", &userInput);
+    
+
+    if(userInput % 2 == 0){
+        printf("\n%d is an even number...\n\n", userInput);
+    } else {
+        printf("\n%d is an odd number...\n\n", userInput);
+    }
+    
+    
+    return 0;
+}
