225d43791a0aaff347f2203b366aac1a43589f31
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-03-21 17:33:13 -0300

    20210321

---
+++ C_Programming_For_Beginners/SECTION_1_C_PROGRAMMING_BASICS/PRINTF/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	char adjective[] = "cruel";
+	char noun[] = "world";
+	printf("Goodbye, %s %s!\n", adjective, noun);
+	puts("Puts does not accept or support format specifiers.\n");
+	return (0);
+}
