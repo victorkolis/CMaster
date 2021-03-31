9286d7388f026be90672133958e9d72a74a8550d
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-03-31 11:18:26 -0300

    20210331

---
+++ C_Programming_For_Beginners/SECTION_2_C_PROGRAMMING_BASICS/HELLO_WORLD/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* This is a c standard comment */
+// This is a c++ single line comment
+
+main() {
+	printf("Hello Strange World!\n");
+}
---
+++ C_Programming_For_Beginners/SECTION_2_C_PROGRAMMING_BASICS/VARIABLES_AND_TYPES/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	/* Variables */
+	double my_old_salary;
+	double my_new_salary;
+	
+	double my_height, my_weight;
+	
+	my_old_salary = 1550.37;
+	my_new_salary = 3000.00;
+	my_height = 1.74;
+	my_weight = 82.22;
+	
+	printf("my_old_salary = %.2f\n", my_old_salary);
+	printf("my_new_salary = %.2f\n", my_new_salary);
+	printf("my_height = %.2f\n", my_height);
+	printf("my_weight = %.2f\n", my_weight);
+	
+}
