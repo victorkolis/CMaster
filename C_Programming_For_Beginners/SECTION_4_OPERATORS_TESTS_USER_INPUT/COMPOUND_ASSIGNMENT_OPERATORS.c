c9c33965a9b2643c3db79aa4eb4e8a8f4b5a9cbf
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-03-30 23:32:24 -0300

    20210330

---
+++ C_Programming_For_Beginners/SECTION_4_OPERATORS_TESTS_USER_INPUT/COMPOUND_ASSIGNMENT_OPERATORS.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int a = 3; /* Assigment */
+	a += 2; /* Compound */
+	a++;  /* Incremental */
+	
+	
+	printf("%d\n", a);
+	
+	return 0;
+}
