d77f202f286470d8ae783f4868c51b2aeac027cc
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-03-31 16:13:27 -0300

    20210331

---
+++ AlphaTech/SECTION_4_WELCOME_FIRST_PROGRAM/PRINT_NAME_AGE_GENDER_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("Full name: Victor Kolis\n");
+	printf("Age: 27 years old\n");
+	printf("Gender: Male\n");
+	
+	return 0;
+}
---
+++ AlphaTech/SECTION_4_WELCOME_FIRST_PROGRAM/PRINT_NAME_AGE_GENDER_2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("Full name: Victor Kolis \nAge: 26 years old \nGender: Male");
+}
