919b59efce9c83f388e2922178b3055740b7a427
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-04-01 15:08:29 -0300

    20210401

---
+++ AlphaTech/SECTION_5_COMMENTS_AND_FORMAT_SPECIFICATION/COMMENTS.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("This is a comments program\n");
+	puts("// -> single-liners");
+	puts("/**/ -> multi-liners");
+	
+	/* C comment */
+	// C++ comment
+	
+	return 0;
+}
---
+++ AlphaTech/SECTION_5_COMMENTS_AND_FORMAT_SPECIFICATION/FORMAT_SPECIFICATION.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	/* INTEGERS */
+	printf("I am %d years old.\n", 27);
+	
+	/* FLOATS */
+	printf("π = %.4f\n", 3.14159);
+
+	
+	return EXIT_SUCCESS;
+}
