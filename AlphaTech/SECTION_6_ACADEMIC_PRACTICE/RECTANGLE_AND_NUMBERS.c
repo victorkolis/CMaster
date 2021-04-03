a95be6fca15fd120ed5bda29647141c4f9d04e2b
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-04-03 18:44:56 -0300

    20210403

---
+++ AlphaTech/SECTION_6_ACADEMIC_PRACTICE/RECTANGLE_AND_NUMBERS.c
@@ -0,0 +1,18 @@
+// Print a 10 by 5 rectangle mixing numbers
+// 1*********
+// 12********
+// 123*******
+// 1234******
+// 12345*****
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	puts("1*********");
+	puts("12********");
+	puts("123*******");
+	puts("1234******");
+	puts("12345*****");
+	
+	return 0;
+}
---
+++ AlphaTech/SECTION_6_ACADEMIC_PRACTICE/YEAR_I_WAS_BORN.c
@@ -0,0 +1,13 @@
+/* Print the year you were born by subtracting the current year
+ * by your current age
+ */
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	printf("%d", 2021 - 27);
+	
+	return 0;
+}
