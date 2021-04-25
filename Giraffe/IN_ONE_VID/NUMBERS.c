5e6198abb567026b4d873aa8395d672326253018
Victor Kolis <58838266+victorkolis@users.noreply.github.com>
2021-04-24 21:56:00 -0300

    20210424

---
+++ Giraffe/IN_ONE_VID/NUMBERS.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	
+	// N U M B E R S
+	
+	printf("%d \n", 2 * 3);
+	printf("%d \n", 10 % 3);
+	printf("%d \n", 1 + 2 * 3);
+	printf("%f \n", 10 / 3.0);
+	
+	
+	int num = 10;
+	num += 100;
+	printf("%d \n", num);
+	
+	
+	return 0;
+	
+}
